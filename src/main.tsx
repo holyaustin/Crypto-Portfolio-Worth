0dd4e68d751e9fc951aa01de3cb7b5e610c4ef3e
holyaustin <holyaustin@yahoo.com>
2024-10-31 20:52:27 +0100

    button working commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.tsx'
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import App from './App.tsx'
 import Welcome from './Welcome.jsx'
 import './index.css'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Welcome />
+     <BrowserRouter>
+    <Routes>
+      <Route path="/" element={ <Welcome />} />
+      <Route path="app" element={<App />} />
+      {/**
+      <Route path="about" element={<About />} />
+      <Route path="create" element={<Create />} />
+      <Welcome />
+      */}
+    </Routes>
+    </BrowserRouter>
+   
   </React.StrictMode>,
 )
