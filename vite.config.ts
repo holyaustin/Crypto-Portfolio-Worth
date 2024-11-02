b15128100e4a7b038dd4ab88a009d8c581d502f3
holyaustin <holyaustin@yahoo.com>
2024-11-02 09:53:08 +0100

    barrier

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 //import { resolve } from 'path';
 
+
 //const root = resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
